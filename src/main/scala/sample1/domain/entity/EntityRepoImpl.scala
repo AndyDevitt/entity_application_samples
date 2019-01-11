b8c31340d1c3c99b080381bf45d707bd6e928eff
Andy Devitt <adevitt@mdsol.com>
2019-01-11 15:20:43 +0000

    - tidy up and rename for clarity

--- src/main/scala/sample1/domain/entity/EntityRepoImpl.scala
+++ src/main/scala/sample1/domain/entity/EntityRepoImpl.scala
@@ -9,20 +9,16 @@ trait EntityRepoImpl[F[_], IdType <: EntityId, EntType <: VersionedEntity[EntTyp
 
   def persistenceRepo: PersistenceRepo[F, ErrType, PersType, PersIdType]
 
-  def saveEntity(v: EntType)
-                (implicit monad: Monad[F],
-                 versioned: Versioned[EntType],
-                 codec: Codec[EntType, PersType, ErrType]
+  def saveEntity(entity: EntType)
+                (implicit monad: Monad[F], versioned: Versioned[EntType], codec: Codec[EntType, PersType, ErrType]
                 ): F[Either[ErrType, EntType]] = {
-    val newVer = versioned.incrementVersion(v)
+    val newVer = versioned.incrementVersion(entity)
     monad.map(persistenceRepo.save(codec.encode(newVer)))(
       _.map(_ => cleanEntityForResultAfterSaveF(newVer)))
   }
 
   def retrieveEntity(id: IdType)
-                    (implicit monad: Monad[F],
-                     codec: Codec[EntType, PersType, ErrType],
-                     idEncoder: Encoder[IdType, PersIdType]
+                    (implicit monad: Monad[F], codec: Codec[EntType, PersType, ErrType], idEncoder: Encoder[IdType, PersIdType]
                     ): F[Either[ErrType, EntType]] =
     monad.map(persistenceRepo.retrieve(idEncoder.encode(id)))(
       _.flatMap(codec.decode))
