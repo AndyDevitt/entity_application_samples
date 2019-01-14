d89ed86f8d92dc70823ec453c1916c21706e4326
Andy Devitt <adevitt@mdsol.com>
2019-01-14 20:09:21 +0000

    - add generic EntityApplication trait and update InvoiceApplication to extend it

---
+++ src/main/scala/sample1/application/EntityApplication.scala
@@ -0,0 +1,26 @@
+package sample1.application
+
+import cats.{Monad, ~>}
+import sample1.domain.command.{CommandG, CommandInput}
+
+trait EntityApplication[F[_], G[_], I <: CommandInput, Error] {
+
+  def input: I
+
+  /**
+    * Process a generic command.
+    *
+    * @param cmd                   the command to be processed
+    * @param monadF                monad instance for the application context type
+    * @param monadG                monad instance for the underlying repository context type
+    * @param naturalTransformation natural transformation from the repository context (G) to the application context (F)
+    * @tparam R the successful result type of the command
+    * @return returns the result of running the command
+    */
+  def processCommand[R](cmd: CommandG[G, I, R, Error])
+                       (implicit monadF: Monad[F],
+                        monadG: Monad[G],
+                        naturalTransformation: G ~> F
+                       ): F[Either[Error, R]] =
+    cmd.run(input)
+}
--- src/main/scala/sample1/application/InvoiceApplicationWithProcessor.scala
+++ src/main/scala/sample1/application/InvoiceApplicationWithProcessor.scala
@@ -1,41 +1,39 @@
 package sample1.application
 
+import cats.Id
 import cats.effect.IO
-import cats.{Id, Monad, ~>}
 import sample1.domain.command._
 import sample1.domain.cta.CtaRepo
 import sample1.domain.{InvoiceError, InvoiceRepo}
 
 import scala.concurrent.Future
 
-trait InvoiceApplicationWithProcessor[F[_], G[_]] {
-
-  def repo: InvoiceRepo[G]
+/**
+  * Invoice Application which extends EntityApplication and simply needs to define the input type and construct it
+  *
+  * @tparam F the context for the application
+  * @tparam G the context for the underlying repo
+  */
+trait InvoiceApplicationWithProcessor[F[_], G[_]] extends EntityApplication[F, G, DomainCommandInput[G], InvoiceError] {
+  def invoiceRepo: InvoiceRepo[G]
 
   def ctaRepo: CtaRepo[G]
 
-  final val input: DomainCommandInput[G] = new DomainCommandInput[G](repo, ctaRepo)
-
-  /**
-    * Process a generic command.
-    *
-    * @param cmd                   the command to be processed
-    * @param monadF                monad instance for the underlying repository context type
-    * @param monadG                monad instance for the application context type
-    * @param naturalTransformation natural transformation from the repository context (F) to the application context (G)
-    * @param transformer           transformer instance to convert from the command result type to the application result type
-    * @tparam R the successful result type of the command
-    * @tparam T the required sucess result type for the caller
-    * @return returns the result of running the command, transformed into the callers desired type
-    */
-  def processCommand[R](cmd: CommandG[G, DomainCommandInput[G], R, InvoiceError])
-                       (implicit monadF: Monad[F],
-                        monadG: Monad[G],
-                        naturalTransformation: G ~> F
-                       ): F[Either[InvoiceError, R]] =
-    cmd.run(input)
+  override final val input: DomainCommandInput[G] = new DomainCommandInput[G](invoiceRepo, ctaRepo)
 }
 
-class ProdApplicationWithProcessor(override val repo: InvoiceRepo[IO], override val ctaRepo: CtaRepo[IO]) extends InvoiceApplicationWithProcessor[Future, IO]
+/**
+  * An example production application using Future and IO contexts for the application and repo respectively
+  *
+  * @param invoiceRepo
+  * @param ctaRepo
+  */
+class ProdApplicationWithProcessor(override val invoiceRepo: InvoiceRepo[IO], override val ctaRepo: CtaRepo[IO]) extends InvoiceApplicationWithProcessor[Future, IO]
 
-class TestApplicationWithProcessor(override val repo: InvoiceRepo[Id], override val ctaRepo: CtaRepo[Id]) extends InvoiceApplicationWithProcessor[Id, Id]
+/**
+  * An example test repo that simply uses the Id context
+  *
+  * @param invoiceRepo
+  * @param ctaRepo
+  */
+class TestApplicationWithProcessor(override val invoiceRepo: InvoiceRepo[Id], override val ctaRepo: CtaRepo[Id]) extends InvoiceApplicationWithProcessor[Id, Id]
