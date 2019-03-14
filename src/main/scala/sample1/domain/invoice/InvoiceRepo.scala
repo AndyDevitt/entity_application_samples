4e989907f43dfd279e55352ba84a1a846be00652
Andy Devitt <adevitt@mdsol.com>
2019-03-14 23:05:24 +0000

    - move invoice repo into invoice package

--- src/main/scala/sample1/domain/invoice/InvoiceRepo.scala
+++ src/main/scala/sample1/domain/invoice/InvoiceRepo.scala
@@ -1,8 +1,7 @@
-package sample1.domain
+package sample1.domain.invoice
 
 import sample1.domain.entity.EntityRepo
 import sample1.domain.errors.InvoiceError
-import sample1.domain.invoice.{Invoice, InvoiceId}
 
 trait InvoiceRepo[F[_]] extends EntityRepo[F, InvoiceId, Invoice, InvoiceError] {
   def find(): F[Either[InvoiceError, Seq[Invoice]]]
